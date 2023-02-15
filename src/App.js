a7b162681120f58a779c11f8672188b28de1df6b
Mirian Rodriguez <mirodriiguez8@gmail.com>
2023-02-15 17:30:34 -0300

    Se creó el proyecto. Se añadieron las librerías necesarias. La aplicación permite iniciar sesión con un usuario de google y disparar la creación de un documento en su Drive

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { GoogleLoginBtn } from './components/GoogleLoginBtn';
+import { gapi } from 'gapi-script';
+import { useEffect } from 'react';
+
+const CLIENT_ID='554296606023-876meuumt5vmou76tep6guchu2tucqet.apps.googleusercontent.com'
+const API_KEY='AIzaSyCW2mXqEdp0gZ8ov4UCUvnNANJmu9Hm-kg'
+const SCOPES='https://www.googleapis.com/auth/documents https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file'
 
 function App() {
+
+  useEffect(() => {
+    function start(){
+      gapi.client.init({ 
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        scope: SCOPES,    
+      })
+    };
+    gapi.load('client:auth2', start);
+  })
+
+  const createFile = (title) => {
+    //const accessToken = gapi.auth.getToken().access_token;
+    const accessToken = gapi.client.getToken().access_token;
+    console.log("access token" + accessToken);
+    fetch('https://docs.googleapis.com/v1/documents?title='+title, {
+      method:"POST",
+      headers: { 'Authorization': 'Bearer ' + accessToken},
+    }).then( (res) => {
+      return res.json();
+    }).then(function(val){
+      console.log(val);
+      console.log(val.documentId);
+      window.open('https://docs.google.com/document/d/' +val.documentId+ '/edit', '_blank');
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GoogleLoginBtn/>
+      <button onClick={()=>createFile("Mi documento desde React")}>
+        Crear documento
+      </button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="554296606023-876meuumt5vmou76tep6guchu2tucqet.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
